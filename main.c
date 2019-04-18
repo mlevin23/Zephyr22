535b5f884cfcd4964755cf563fab9340c5308472
Kyle Meyer <42076074+kmeyer1212@users.noreply.github.com>
2019-04-18 14:09:50 -0500

    Rough code for case 1

--- main.c
+++ main.c
@@ -4,13 +4,24 @@
 
 void main(){
     int choice = 0;
+    float newTMP = 0.0;
+    float currentTMP = 0.0;
     printk("What would you like to do? %s\n");
     printk("1. Adjust Temperature %s\n");
     printk("2. Adjust TOD Temperature %s\n");
     scanf("%d", choice);
   
     switch(choice){
-      case 1: //TODO
+      case 1:
+            printk("What would you like to change the temp to?%s\n");
+            scanf("%f", newTMP); //Since we are using printk, do we need to use scank instead of scanf?
+            if(newTMP > currentTMP){
+                //ACTIVATE HEATING;
+            }
+            else if(newTMP < currentTMP){
+                //ACTIVATE COOLING;
+            }
+            break;            
       case 2: //TODO
     }
 }
