8bbd2b0063b0bc26bbf69781a234c6e100f74bae
Kyle Meyer <42076074+kmeyer1212@users.noreply.github.com>
2019-04-17 19:53:34 -0500

    Added basic skeleton

--- main.c
+++ main.c
@@ -3,5 +3,14 @@
 #include <time.h>
 
 void main(){
-
+    int choice = 0;
+    printk("What would you like to do? %s\n");
+    printk("1. Adjust Temperature %s\n");
+    printk("2. Adjust TOD Temperature %s\n");
+    scanf("%d", choice);
+  
+    switch(choice){
+      case 1: //TODO
+      case 2: //TODO
+    }
 }
