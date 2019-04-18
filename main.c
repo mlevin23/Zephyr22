97100bfd3faf58d2d9afaea7d7666cedda5827ab
CMPerniciaro <47083006+CMPerniciaro@users.noreply.github.com>
2019-04-18 14:34:32 -0500

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ void main(){
     int choice = 0; //int
     long newTMP = 0.0; //float
     long currentTMP = 68.0; //Good starting point to initialize the temprature
+    long adjTMP = 0.0;
     
     printk("What would you like to do? %s\n");
     printk("1. Adjust Temperature %s\n");
@@ -25,6 +26,25 @@ void main(){
             break;            
       case 2:
             printk("How would you like to adjust the TOD?%s\n");
+            printk("Would you like to adjust the \n1. Morning Temperature\n2.Afternoon Temperature\n3.Evening Temperature%s\n?");
+            scanf("%d", choice2);
+            
+            switch(choice2){
+                    case 1: // Morning
+                        printk("What would you like to adjust the morning temperature to?%s");
+                        scanf("%d", adjTMP);
+                        for ( // TOD = MORNING ) {
+                            int adjTMPVAL = adjTMP;
+                            adjTMPVAL = currentTMP;
+                        }
+                        
+                    
+                    break;
+                    case 2: // Afternoon
+                    case 3: //Evening
+                    
+            break;
+               
             //Do we want to create a menu here?
             //How should we adjust the TOD temprature?            
     }
