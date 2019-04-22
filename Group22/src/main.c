1ddd1a4c8e0eafab39c334024b076c0b8b7bcbf7
Mlevin23 <32828119+mlevin23@users.noreply.github.com>
2019-04-22 14:52:55 -0500

    Add files via upload

--- Group22/src/main.c
+++ Group22/src/main.c
@@ -28,19 +28,62 @@ void main(void) {
 	switch(choice){
       		case 1:
             		printk("What would you like to change the temp to?\n");
-            		char *c = console_getline();
-			newTMP = atoi(c);
+            		*s = console_getline();
+			newTMP = atoi(s);
 			if(newTMP > currentTMP){
                 //ACTIVATE HEATING;
-                	printk("am heating");
+                	printk("am heating\n");
             		}
             	else if(newTMP < currentTMP){
                 //ACTIVATE COOLING;
-            		printk("am cooling");
+            		printk("am cooling\n");
             		}
 		break; 
 
-		}
-	printk("done");
-	return 0;
+	case 2:
+            printk("How would you like to adjust the TOD?\n");
+            printk("Would you like to adjust the \n1. Morning Temperature\n2.Afternoon Temperature\n3.Evening Temperature\n?");
+            *s = console_getline();
+            choice2 = atoi(s);
+
+            
+		switch(choice2){
+			case 1: // M0RNING 
+			printk("What would you like to adjust the morning temperature to?");
+			*s = console_getline();
+			adjTMP = atoi(s);
+			if ( 0 < tm_hour && tm_hour <= 11 ) {
+				newADJTMP = changeADJTMP(adjTMP);    
+			}    
+			break;
+                            
+			case 2: // AFTERNOON
+			printk("What would you like to adjust the morning temperature to?");
+			*s = console_getline();
+			adjTMP = atoi(s);
+			if ( 11 < tm_hour && tm_hour <= 17 ) {
+				newADJTMP = changeADJTMP(adjTMP);
+			} 
+			break;
+                          
+			case 3: // EVENING
+			printk("What would you like to adjust the morning temperature to?");
+			*s = console_getline();
+			adjTMP = atoi(s);
+			if ( 17 < tm_hour && tm_hour <= 23) {
+				newADJTMP = changeADJTMP(adjTMP);            
+			}    
+                	break;
+                            
+        	break;            
+		}	
+	}
+        printk("done");
+}
+
+
+long changeADJTMP(long adjTMP){
+    long adjTMPVAL = adjTMP;
+    // TODO
+    return adjTMPVAL;
 }
