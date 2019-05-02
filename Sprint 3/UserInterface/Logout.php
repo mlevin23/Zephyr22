2412c1198a4a42daf9373ff75c732e5614eb9c44
CMPerniciaro <47083006+CMPerniciaro@users.noreply.github.com>
2019-05-02 14:25:46 -0500

    Create Logout.php

---
+++ Sprint 3/UserInterface/Logout.php
@@ -0,0 +1,23 @@
+<?php
+
+	
+	if(!session_start()) {
+		header("Location: Cmpvm5Final_Error.php");
+		exit;
+	}
+
+	$_SESSION = array();
+
+	if (ini_get("session.use_cookies")) {
+		$params = session_get_cookie_params();
+		setcookie(session_name(), '', 1,
+			$params["path"], $params["domain"],
+			$params["secure"], $params["httponly"]
+		);
+	}
+	
+	session_destroy();
+	
+	header("Location: index.php");
+	exit;
+?>
