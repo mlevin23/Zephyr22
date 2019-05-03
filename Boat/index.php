83d428c80e1da8b3bba135d7dd2ebb380005962c
Kyle Meyer <42076074+kmeyer1212@users.noreply.github.com>
2019-05-02 20:25:08 -0500

    Files for you to look over Maxim

---
+++ Boat/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+    <meta charset="UTF-8">
+    <link href="Styles/website.css" rel="stylesheet" type="text/css">
+    <link href="myJQuery/jquery-ui.min.css" rel="stylesheet" type="text/css">
+    <script src="myJQuery/external/jquery/jquery.js"></script>
+    <script src="myJQuery/jquery-ui.min.js"></script>    
+</head>
+<body class="backGround">
+    <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a href="News.php">Learn</a>
+        <a href="Contact.php">Contact</a>
+        <a href="About.php">About</a>
+    </div>
+    
+    <div>
+        <h1 class="header">Meyer Boat Cleaning</h1>
+    </div>
+    
+    <div id="homePlaque">
+        <p>Welcome! Please use the bar above to navigate the site and learn what we have to offer! If interested, please do not hesitate to contact me! Thanks!</p>
+    </div>
+    
+    <div>
+        <img class="imgHome" src="images/me.jpg" alt="Me" align="middle">
+        <img class="imgHome" src="images/BaA.JPG" alt="Before and after" align="middle">
+        <img class="imgHome" src="images/marina.jpg" alt="marina" align="middle">
+    </div>
+</body>
+</html>
+    
