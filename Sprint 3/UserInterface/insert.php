5dab02f5500f5e85d90bddccacf00316a3ad8e85
Mlevin23 <32828119+mlevin23@users.noreply.github.com>
2019-05-15 12:30:25 -0500

    Update insert.php

--- Sprint 3/UserInterface/insert.php
+++ Sprint 3/UserInterface/insert.php
@@ -1,7 +1,14 @@
 <?php
 $name= $_POST['name'];
 $value= $_POST['value'];
+$type= $_POST['type'];
 
-$txt = "The button ".$name." and is ".$value;
+if($type == 'light') {
+    if($value)
+        $txt= "The light ".$name." is on";
+    else
+        $txt="The light ".$name." is off";
+}
+
 $myfile = file_put_contents('data.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
 ?>
