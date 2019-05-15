1828140547613d5deb5bdb3152669f93279c0431
Kyle <kbmeyer1997@yahoo.com>
2019-05-15 09:18:24 -0500

    Carolyn's code for temperature page & code to tweak appliances

--- Sprint 3/UserInterface/Appliances.php
+++ Sprint 3/UserInterface/Appliances.php
@@ -4,6 +4,7 @@
 	<title>Appliances</title>
     <meta charset="UTF-8">
     <link href="Styles/Zephyr.css" rel="stylesheet" type="text/css">
+    <link href="Styles/newCSS.css" rel="stylesheet" type="text/css">
 </head>
 <body class="backGround">
     <div class="topnav">
@@ -17,26 +18,28 @@
         <h1 class="header">Appliances</h1>
     </div>
     
-    <div class="boxed">
-        Oven
-        <label class="switch">
-            <input type="checkbox">
-            <span class="slider round"></span>
-        </label>
-    </div>
-    <div class="boxed">
-        Coffee Maker
-        <label class="switch">
-            <input type="checkbox">
-            <span class="slider round"></span>
-        </label>
-    </div>
-    <div class="boxed">
-        Bottle Warmer
-        <label class="switch">
-            <input type="checkbox">
-            <span class="slider round"></span>
-        </label>
+    <div class="center">
+        <div class="boxed">
+            Oven
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label>
+        </div>
+        <div class="boxed">
+            Coffee Maker
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label>
+        </div>
+        <div class="boxed">
+            Bottle Warmer
+            <label class="switch">
+                <input type="checkbox">
+                <span class="slider round"></span>
+            </label>
+        </div>
     </div>
 </body>
 </html>
