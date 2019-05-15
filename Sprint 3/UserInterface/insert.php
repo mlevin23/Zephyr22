72ade8b78fe512007c068aed3877e3b8a82e32d5
Mlevin23 <32828119+mlevin23@users.noreply.github.com>
2019-05-15 12:41:06 -0500

    Update insert.php

--- Sprint 3/UserInterface/insert.php
+++ Sprint 3/UserInterface/insert.php
@@ -4,10 +4,27 @@ $value= $_POST['value'];
 $type= $_POST['type'];
 
 if($type == 'light') {
-    if($value)
+    if($value == 'true'){
         $txt= "The light ".$name." is on";
-    else
+    }
+    if($value == 'false')
+    {
         $txt="The light ".$name." is off";
+    }
+}
+
+if($type == 'appliance'){
+    if($value == 'true'){
+        $txt= "The ".$name." is on";
+    }
+    if($value == 'false')
+    {
+        $txt="The ".$name." is off";
+    }
+}
+
+if($type == 'temperature'){
+    $txt = "The temperature for ".$name." is set to ".$valuel;
 }
 
 $myfile = file_put_contents('data.txt', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
