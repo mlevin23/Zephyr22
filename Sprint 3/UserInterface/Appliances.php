ef1a79f9c2e8b7ab822ec36096016166c3a5f9cd
Kyle <kbmeyer1997@yahoo.com>
2019-05-15 13:02:50 -0500

    Added appliance log information/enabled

--- Sprint 3/UserInterface/Appliances.php
+++ Sprint 3/UserInterface/Appliances.php
@@ -5,6 +5,17 @@
     <meta charset="UTF-8">
     <link href="Styles/Zephyr.css" rel="stylesheet" type="text/css">
     <link href="Styles/newCSS.css" rel="stylesheet" type="text/css">
+    <script src="jquery-2.1.0.min.js"></script>
+    <script>
+        function create(ID, TYPE) {    
+            $.ajax({
+                url: 'insert.php',
+                type: 'POST',
+                data: { name: ID, value: document.getElementById(ID).checked, type: TYPE},
+                success: function(response) { console.log(response); }
+            });
+        }
+    </script>
 </head>
 <body class="backGround">
     <div class="topnav">
@@ -23,21 +34,21 @@
         <div class="boxed">
             Oven
             <label class="switch">
-                <input type="checkbox">
+                <input onclick="create('oven', 'appliance')" id="oven" type="checkbox">
                 <span class="slider round"></span>
             </label>
         </div>
         <div class="boxed">
             Coffee Maker
             <label class="switch">
-                <input type="checkbox">
+                <input onclick="create('coffee maker', 'appliance')" id="coffee maker" type="checkbox">
                 <span class="slider round"></span>
             </label>
         </div>
         <div class="boxed">
             Bottle Warmer
             <label class="switch">
-                <input type="checkbox">
+                <input onclick="create('bottle warmer', 'appliance')" id="bottle warmer" type="checkbox">
                 <span class="slider round"></span>
             </label>
         </div>
